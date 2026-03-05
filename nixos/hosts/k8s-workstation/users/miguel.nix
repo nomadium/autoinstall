222f3f99b8bc17f93dd52121e8162c5a8a4e44fd
Miguel Landaeta <miguel@miguel.cc>
2026-03-05 17:13:48 +0000

    Update authorized ssh key

--- nixos/hosts/k8s-workstation/users/miguel.nix
+++ nixos/hosts/k8s-workstation/users/miguel.nix
@@ -10,7 +10,7 @@
     shell = pkgs.bash;
     hashedPassword = "$6$AsTOJpwTSLO/njOS$uovfoCmMk5oX27igKI2PRlfFB/Wukz.lacJjrMHixPRUmytsng.m4pzvQmIGB2xD0agc1CjhahcTJhNAT/vxa0";
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNcUSkx11fiGy0VjQD1vr+6kWmXdXxB88lG/6OycxDe somekey"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/wsAto0fPw8EYU3viWowcMcjn6xZTidiWmhSi+sTIV somekey"
     ];
   };
 }
