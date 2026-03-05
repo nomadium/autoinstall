c2a08d42c6dbbaf56f572a9cc289870572116113
Miguel Landaeta <miguel@miguel.cc>
2026-03-05 17:16:07 +0000

    Remove dummy password

--- nixos/hosts/k8s-workstation/users/miguel.nix
+++ nixos/hosts/k8s-workstation/users/miguel.nix
@@ -8,7 +8,7 @@
       tree
     ];
     shell = pkgs.bash;
-    hashedPassword = "$6$AsTOJpwTSLO/njOS$uovfoCmMk5oX27igKI2PRlfFB/Wukz.lacJjrMHixPRUmytsng.m4pzvQmIGB2xD0agc1CjhahcTJhNAT/vxa0";
+    hashedPassword = "!";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/wsAto0fPw8EYU3viWowcMcjn6xZTidiWmhSi+sTIV somekey"
     ];
